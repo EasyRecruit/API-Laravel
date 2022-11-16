feed8eb10a475ab77cfa63a57469381ff31c823e
arhinful <kofibusy@gmail.com>
2022-11-16 11:12:43 +0000

    wip: modify users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,21 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->uuid();
+            $table->foreignId('authenticatable_id')->constrained()->cascadeOnDelete();
+            $table->string('authenticatable_type');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('other_names');
+            $table->string('mobile_number')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->boolean('is_active')->default(true);
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
