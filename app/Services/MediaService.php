51bf12d905c83f3e5aca601b91be033287962647
arhinful <kofibusy@gmail.com>
2022-11-21 14:15:27 +0000

    wip: add/delete/update worker

--- app/Services/MediaService.php
+++ app/Services/MediaService.php
@@ -7,7 +7,6 @@ namespace App\Services;
 class MediaService
 {
     public function storePdf($owner, $file){
-//        $path = uploadFileTo($file, 'cv');
         $owner->addMedia($file)->toMediaCollection('cv');
     }
 }
