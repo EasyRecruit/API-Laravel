30aeac5cd60210c582c1fc7cfb181446d02b0260
arhinful <kofibusy@gmail.com>
2022-11-21 14:34:10 +0000

    wip: crud company

--- routes/api/v1/company.php
+++ routes/api/v1/company.php
@@ -5,8 +5,17 @@ use App\Http\Controllers\V1\CompanyController;
 use Illuminate\Support\Facades\Route;
 
 
+Route::post('company/register', [CompanyController::class, 'store']);
+
+
 Route::prefix('company')
     ->controller(CompanyController::class)
+    ->middleware(['auth:sanctum', 'active'])
     ->group(function (){
-        Route::post('register', 'store');
+        Route::get('/', 'index')->middleware(['admin']);
+        Route::get('/{company:uuid}', 'show');
+        Route::patch('/{company:uuid}', 'update')->middleware(['company']);
+        Route::delete('/self', 'destroy')->middleware(['company']);
     });
+
+
